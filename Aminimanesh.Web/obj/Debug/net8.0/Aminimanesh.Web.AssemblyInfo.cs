42e92f7aa0f99d8f14823f9df6670e9db3262359
Aminimanesh <aztecgoodamin1@gmail.com>
2024-07-12 17:39:17 +0330

    Make seo better 2

--- Aminimanesh.Web/obj/Debug/net8.0/Aminimanesh.Web.AssemblyInfo.cs
+++ Aminimanesh.Web/obj/Debug/net8.0/Aminimanesh.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aminimanesh.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31077a560da675892cfbad3ab3825214a13fbb0b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45ccf735ab817ba36dfb6da12dc920b739abd0cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aminimanesh.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aminimanesh.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
